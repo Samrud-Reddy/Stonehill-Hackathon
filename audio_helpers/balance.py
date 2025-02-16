c2e0941b1ad3edb24c76fceec031ca516c5ea467
Samrud Reddy <samrudr@gmail.com>
2025-02-16 16:27:42 +0530

    Removed files that are never used

--- audio_helpers/balance.py
+++
@@ -1,8 +0,0 @@
-from gtts import gTTS
-import os
-
-mytext = 'Check your Balance'
-language = 'en'
-myobj = gTTS(text=mytext, lang=language, slow=False)
-myobj.save("pre_recorded2.mp3")
-os.system("start pre_recorded2.mp3")
